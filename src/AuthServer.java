1c6b3651be4cea7a23ba122f8c930f0f058443cd
hsadhvani <hsadhvani@gmail.com>
2014-12-05 18:22:43 -0500

    changes

---
+++ src/AuthServer.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class AuthServer {
+	public static void main(String[] args) throws ClassNotFoundException,
+			SQLException {
+		Connection connect = null;
+		Statement statement = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		
+		String host = "localhost:3307";
+		String username = "root";
+		String password = "";
+		if(args.length==3){
+			
+		}
+			
+		Class.forName("com.mysql.jdbc.Driver");
+		connect = DriverManager
+				.getConnection("jdbc:mysql://localhost:3307/test?"
+						+ "user="+username+"&password="+password);
+		statement = connect.createStatement();
+		resultSet = statement
+				.executeQuery("select count(1) as cnt from chat.userinfo where username='binit' and password='1630937c3d00b4f4b153599d93469963'");
+		
+	}
+}
